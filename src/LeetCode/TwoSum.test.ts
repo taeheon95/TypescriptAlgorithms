4ffbccbde93c68aaf19344e68a5bf2de3ebd3b50
taeheon <dalo952003@gmail.com>
2022-02-24 22:20:31 +0900

    아직 풀지 못한 문제들

--- src/LeetCode/TwoSum.test.ts
+++ src/LeetCode/TwoSum.test.ts
@@ -1,3 +1,19 @@
-import TwoSum from './TwoSum';
+import twoSum from './TwoSum';
 
-describe('');
+describe('two sum 테스트', () => {
+  it('case #1', () => {
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    expect(twoSum(nums, target)).toStrictEqual([0, 1]);
+  });
+  it('case #2', () => {
+    const nums = [3, 2, 4];
+    const target = 6;
+    expect(twoSum(nums, target)).toStrictEqual([1, 2]);
+  });
+  it('case #3', () => {
+    const nums = [3, 3];
+    const target = 6;
+    expect(twoSum(nums, target)).toStrictEqual([0, 1]);
+  });
+});
--- src/LeetCode/TwoSum.ts
+++ src/LeetCode/TwoSum.ts
@@ -1,3 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
   return [];
 }
+
+export default twoSum;
